890bcc1291eb87bab02ab55fbb8e0756c616c4ef
Prince-Sandhu <95694158+Prince-Sandhu@users.noreply.github.com>
2022-01-10 20:46:56 -0500

    Update RegexExc.java

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
@@ -2,14 +2,14 @@ package ca.jrvs.apps.practice;
 
 public interface RegexExc {
     /**
-     * return true if filename extension is jpg or jpeg (case-insensitive).
+     * Return true if filename extension is jpg or jpeg (case-insensitive).
      * @param filename
      * @return
      */
     public boolean matchJpeg(String filename);
 
     /**
-     * return true if ip is valid
+     * Return true if ip is valid
      * IP address range is from 0.0.0.0 to 999.999.999.999
      * @param ip
      * @return
@@ -17,7 +17,7 @@ public interface RegexExc {
     public boolean matchIp(String ip);
 
     /**
-     * return true if line is empty (empty, whitespace, tab)
+     * Return true if line is empty (empty, whitespace, tab, etc.).
      * @param line
      * @return
      */
